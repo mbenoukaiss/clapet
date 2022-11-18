1ee9b58798ee4c0c5f28b69352696980a38cc427
Marwane <mbenoukaiss@gmail.com>
2022-11-18 12:41:43 +0100

    Initial Commit

---
+++ Sleepless/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sleepless
+//
+//  Created by Marwane BENOU-KAÏSS on 18/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sleepless/SleeplessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleeplessApp.swift
+//  Sleepless
+//
+//  Created by Marwane BENOU-KAÏSS on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SleeplessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
